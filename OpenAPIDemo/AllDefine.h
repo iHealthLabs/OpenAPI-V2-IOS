c31f095cf41eb46645835029b65a1fe627138b27
iHealth <webmaster@ihealthlabs.com>
2013-09-26 17:27:08 +0800

    Update AllDefine.h

--- OpenAPIDemo/AllDefine.h
+++ OpenAPIDemo/AllDefine.h
@@ -16,25 +16,22 @@
 #define kAdKeychainAPIName              @"AndonAPIName"
 #define kAdKeychainUserID               @"AndonUserID"
 
-//测试服务器
-#define kAuthURL @"http://testtokyo.ihealthlabs.com:8000/OpenApiV2/OAuthv2/userauthorization/"
-#define kBaseInfoURL @"http://testtokyo.ihealthlabs.com:8000/openapiv2/user/"
 
-/*
+
  //正式服务器
- #define kAuthURL @"http://api.ihealthlabs.com:8443/OpenApiV2/OAuthv2/userauthorization/"
- #define kBaseInfoURL @"http://api.ihealthlabs.com:8443/openapiv2/user/"
+ #define kAuthURL @"https://api.ihealthlabs.com:8443/OpenApiV2/OAuthv2/userauthorization/"
+ #define kBaseInfoURL @"https://api.ihealthlabs.com:8443/openapiv2/user/"
  
-*/
+
 
 //使用者需要更改的。使用时，到网站申请应用，换成应用的SC,SV
-#define mysc @"9c6830141b7e43908895de2b1efe4424"
-#define myweightsv @"34e038a2f5344abe8c748f2952042f22"
-#define mybpsv @"02a746d4fe464c6689eb577b9c835c11"
-#define myOXSV @"4c0eecccb0134104af7c5e4c1a2262dd"
-#define myBGSV @"bf058dbac2be413f8c9e9bd1292548f9"
-#define mySleepSV @"c8e68a19f1b54ce5b899a74e67a65c08"
-#define myActivitySV @"9eabc577831443d284d73cec0811c416"
+#define mysc @"9c6830141b7e43908******"
+#define myweightsv @"34e038a2f5344ab******"
+#define mybpsv @"02a746d4fe464c6689eb******"
+#define myOXSV @"4c0eecccb0134104af******"
+#define myBGSV @"bf058dbac2be413f8c9******"
+#define mySleepSV @"c8e68a19f1b54ce5b******"
+#define myActivitySV @"9eabc577831443d284******"
 #define myredirect_uri @"http://www.ihealthlabs.com"
 //使用者需要更改的
 
